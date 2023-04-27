a0f0c32b4b69ec34e5f9b2b5244e2354df811aaf
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-04-27 11:24:02 -0700

    Exercise-02. [Interactive Coding Exercise] Debugging Practice https://app.codingrooms.com/management/assignments/364786/overview

---
+++ section1/exercises/E02-DebuggingPractice.py
@@ -0,0 +1,14 @@
+# Look at the code in the code editor on the right. There are errors in all of the lines of code. Fix the code so that
+# it runs without errors.
+# https://app.codingrooms.com/management/assignments/364786/overview
+#
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+# Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
