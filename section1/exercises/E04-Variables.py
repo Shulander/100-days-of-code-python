46d1c9473784210f0fda5ff6ac731a9ef0ca89bf
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-04-27 12:14:51 -0700

    Exercise-04. [Interactive Coding Exercise] Variables
    https://app.codingrooms.com/management/assignments/364789/overview

---
+++ section1/exercises/E04-Variables.py
@@ -0,0 +1,22 @@
+# Write a program that switches the values stored in the variables a and b.
+# https://app.codingrooms.com/management/assignments/364789/overview
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+
+tmp = a
+a = b
+b = tmp
+
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
