a6f0c922219719bcfd64c776512c014816e9f65d
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-12 17:00:33 -0300

    S02E24: 24. Number Manipulation and F Strings in Python

---
+++ section2/classes/s03e24-number-manipulation-and-f-strings-in-python.py
@@ -0,0 +1,20 @@
+print(8 / 3)
+# Round
+print(round(8 / 3))
+print(round(8 / 3, 2))
+
+# floor division
+print(8 // 3)
+
+# divide/assign to existing variable
+result = 8 / 2
+result /= 2
+
+print(result)
+
+score = 1
+height = 1.8
+isWinning = True
+
+# f-String
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
