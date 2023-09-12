560edf6cba1d4e916b0290d450085302e9097840
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-12 16:14:12 -0300

    S02E19: 19. Python Primitive Data Types

---
+++ section2/classes/s02e19-python-primitive-data-types.py
@@ -0,0 +1,20 @@
+# Data Types
+# String
+print("String Type")
+print("Hello"[1])
+
+print("123"+"345")
+
+# Integer
+print("Integer Type")
+print(123+345)
+
+print(123_345_789)
+
+# Float
+print("Float Type")
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
