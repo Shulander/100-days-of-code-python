e0b692497d1e600dab9a5fed607e6de8c183a171
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-12 16:22:33 -0300

    S02E20: 20. Type Error, Type Checking and Type Conversion

---
+++ section2/classes/s02e20-type-error-type-checking-and-type-conversion.py
@@ -0,0 +1,14 @@
+num_char = len(input("What is your name?"))
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+
+print(type(num_char))
+print(type(new_num_char))
+
+a = 123
+print(type(a))
+b = float(a)
+print(type(b))
+
+print(70 + float("100.5"))
+print(str(70) + str(100))
