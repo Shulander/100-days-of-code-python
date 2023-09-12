3857bd198176b975cfa15cc3c1fefc423501611f
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-12 16:43:03 -0300

    S02E22: 22. Mathematical Operations in Python

---
+++ section2/classes/s02e22-mathematical-operations-in-python.py
@@ -0,0 +1,13 @@
+print(3 + 5)
+print(5 - 3)
+print(3 * 2)
+print(3 / 5)
+print(3 ** 2)  # power of 3 power of 2
+
+print(3 * 3 + 3 / 3 - 3)
+# 1st 3 * 3: 9 + 3 / 3 - 3
+# 2nd 3 / 3: 9 + 1 - 3
+# 3rd 9 + 1: 10 - 3
+# 4th 10 - 3: 7
+
+print(3 * (3 + 3) / 3 - 3)
