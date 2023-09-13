477c12435a09c78ca97cfe03e6939b3285fb394c
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-13 10:22:41 -0300

    S03E29: 29. Control Flow with if / else and Conditional Operators

---
+++ day03/classes/s03e29-control-flow-with-if-else-and-conditional-operators.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You are tall enough to ride the roller coaster")
+else:
+    print("Sorry, you are too short to ride the roller coaster")
