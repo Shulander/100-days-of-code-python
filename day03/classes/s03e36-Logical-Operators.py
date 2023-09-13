954e0119e29bcd5f342882309e6e769ee55b3b80
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-13 12:23:01 -0300

    S03E36: 36. Logical Operators

---
+++ day03/classes/s03e36-Logical-Operators.py
@@ -0,0 +1,30 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You are tall enough to ride the roller coaster")
+    age = int(input("What is your age? "))
+
+    if age < 12:
+        print("Child tickets are $5.00")
+        total = 5
+    elif age <= 18:
+        print("Youth tickets are $7.00")
+        total = 7
+    elif age <= 18:
+        print("Youth tickets are $7.00")
+        total = 7
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
+        total = 0
+    else:
+        print("Adult tickets are $12.00")
+        total = 12
+
+    wants_photo = input("Do you want a photo taken? Y or N ")
+    if wants_photo == "Y":
+        total += 3
+
+    print(f"Your final bill is {total}")
+else:
+    print("Sorry, you are too short to ride the roller coaster")
