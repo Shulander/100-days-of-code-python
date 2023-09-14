e329c5f501586bbdf7338056ced57ae4cf862fe7
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-14 15:01:48 -0300

    s06e63. Hurdles Challenge using While Loops

---
+++ day06/exercises/e63-HurdlesChallengeUsingWhileLoops.py
@@ -0,0 +1,27 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+#
+# The functions move() and turn_left().
+# The conditions front_is_clear() or wall_in_front(), at_goal(), and their negation.
+# How to use a while loop and an if statement.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
