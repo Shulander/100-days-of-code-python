3167a10befb7f2423ea281f10421f724647b6763
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-14 18:09:21 -0300

    Day 7 Project: The Hangman game
    - 69. Challenge 1 - Picking a Random Words and Checking Answers
    - 71. Challenge 2 - Replacing Blanks with Guesses
    - 73. Challenge 3 - Checking if the Player has Won
    - 75. Challenge 4 - Keeping Track of the Player's Lives
    - 77. Challenge 5 - Improving the User Experience

--- day07/project/day7project-The-Hangman.py
+++ day07/project/day7project-The-Hangman.py
@@ -1,73 +1,23 @@
-# Step 1
 import random
+from replit import clear
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#  Delete this line: word_list = ["ardvark", "baboon", "camel"]
+from hangman_words import word_list
+from hangman_art import logo, stages
 
-word_list = ["aardvark", "baboon", "camel"]
 chosen_word = list(random.choice(word_list))
 word_length = len(chosen_word)
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
-#  Set 'lives' to equal 6.
 # start live count
 lives = 6
+# already used letters
+guessed_letters = []
+
+# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+print(logo)
 
 # create display
 display = []
@@ -84,22 +34,36 @@ def has_lives():
     return lives > 0
 
 
-while not has_won(display) and has_lives():
-    guess = input("Pick a letter: ").lower()
+def read_player_guess():
+    new_guess = input("Pick a letter: ").lower()
+    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+    while new_guess in guessed_letters:
+        print(f"You have already picked the letter '{new_guess}', please chose a new letter")
+        print("Already picked letters: " + ', '.join(guessed_letters))
+        new_guess = input("Pick a letter: ").lower()
+    guessed_letters.append(new_guess)
+    return new_guess
 
+
+while not has_won(display) and has_lives():
+    # read customer's guess
+    guess = read_player_guess()
+
+    clear()
+
+    # Check guessed letter
     for i in range(word_length):
         letter = chosen_word[i]
         if guess == letter:
             display[i] = letter
 
-    # TODO-2: - If guess is not a letter in the chosen_word,
-    #  Then reduce 'lives' by 1.
-    #  If lives goes down to 0 then the game should stop and it should print "You lose."
     if guess not in chosen_word:
-        print(f"letter '{guess}' is not in the word. You have lost a live!")
+        # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+        print(f"You guessed '{guess}', that is not in the word. You lose a live!")
         lives -= 1
 
     print(' '.join(display))
+    # TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(stages[lives])
 
 if has_lives():
