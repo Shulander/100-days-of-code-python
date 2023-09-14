1969e289044f23f898d263abbbb99764f23abc02
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-09-14 17:43:57 -0300

    Day 7 Project: The Hangman game
    - 69. Challenge 1 - Picking a Random Words and Checking Answers
    - 71. Challenge 2 - Replacing Blanks with Guesses
    - 73. Challenge 3 - Checking if the Player has Won
    - 75. Challenge 4 - Keeping Track of the Player's Lives

--- day07/project/day7project-The-Hangman.py
+++ day07/project/day7project-The-Hangman.py
@@ -1,27 +1,90 @@
 # Step 1
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = list(random.choice(word_list))
 word_length = len(chosen_word)
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#  Set 'lives' to equal 6.
+# start live count
+lives = 6
+
+# create display
 display = []
 for i in range(word_length):
     display.append("_")
 print(display)
 
 
-# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all
-#  the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 def has_won(word):
-    if "_" in word:
-        return False
-    return True
+    return "_" not in word
 
 
-while not has_won(display):
+def has_lives():
+    return lives > 0
+
+
+while not has_won(display) and has_lives():
     guess = input("Pick a letter: ").lower()
 
     for i in range(word_length):
@@ -29,6 +92,17 @@ while not has_won(display):
         if guess == letter:
             display[i] = letter
 
-    print(display)
+    # TODO-2: - If guess is not a letter in the chosen_word,
+    #  Then reduce 'lives' by 1.
+    #  If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        print(f"letter '{guess}' is not in the word. You have lost a live!")
+        lives -= 1
 
-print("You win!")
+    print(' '.join(display))
+    print(stages[lives])
+
+if has_lives():
+    print("You win!")
+else:
+    print("You lose!")
